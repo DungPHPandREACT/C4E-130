8608d15c113e0dce08fc17cab91b2c2001833e74
Tiến Dũng <dung2000cn@gmail.com>
2022-09-24 21:42:43 +0700

    lesson 7, javascript

---
+++ Lesson7/app.js
@@ -0,0 +1,57 @@
+// var
+// let
+// const
+
+// var, let / const
+// +Thay đổi được giá trị sau khi khởi tạo
+// +Const không thay đổi được giá trị
+// +Const khi khởi tạo biến phải gán luôn giá trị
+// +Var, let không nhất phải gán giá trị, và nếu không gán giá trị thì kết quả sẽ là undefined
+
+// var / let, const
+// +hoisting (var có, let và const không có)
+// +scope (var global scope, let và const chỉ có phạm vi trong 1 scope)
+
+// Quy tắc đặt tên biến: camelCase
+// let number;
+// let yourName;
+
+// let number = 10;
+// number = 'số mười';
+// // typeof variable
+// console.log(typeof number);
+
+// Các kiểu dữ liệu cơ bản
+// +Number: số
+// +String: chuỗi
+// +Boolean: có 2 giá trị là true hoặc false
+// +Null: kiểu dữ liệu rỗng
+// +Undefined: kiểu dữ liệu chưa xác định
+// Các kiểu dữ liệu khác:
+// Object (đối tượng)
+
+// Toán tử
+// Toán tử toán học
+// +, -, *, /
+// **: toán tử mũ
+// %: toán tử chia lấy dư
+
+// let a = 3;
+
+// a += 2 //(a = a + 2);
+// a -=2 // a = a - 2;
+// a *= 2 // (a = a * 2);
+// a /= 2// (a = a / 2);
+
+// Toán tử so sánh
+// == : so sánh giá trị
+// === : so sánh giá trị và kiểu dữ liệu
+// >, <, >=, <=
+// != : so sánh giá trị
+// !== : so sánh giá trị và kiểu dữ liệu
+
+// a = true;
+// console.log(!a);
+
+// let number = prompt('Mời bạn nhập giá trị: ', 1 - 1000);
+// console.log(number);
